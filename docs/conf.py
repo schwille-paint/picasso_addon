8247dec7f88a9e0393b28c1da11c6cd2ca203c9a
FlorianStehr <florian.stehr@gmail.com>
2020-05-13 10:17:41 +0200

    Renamed to index for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'contents'
+master_doc = 'index'
 
 # General information about the project.
 project = 'picasso_addon'
